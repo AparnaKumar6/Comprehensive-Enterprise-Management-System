5c9723701867df081d89e41ac7ca12aabfb5f6f5
AparnaKumar6 <aparnak612001@gmail.com>
2025-08-01 00:46:26 +0530

    main.py

--- manufacturing_erp_gui/main.py
+++ manufacturing_erp_gui/main.py
@@ -13,16 +13,3 @@ employees = [
 ]
 
 # API Endpoint: Get all employees
-@app.route('/api/employees', methods=['GET'])
-def get_employees():
-    return jsonify(employees)
-
-# API Endpoint: Add new employee
-@app.route('/api/employees', methods=['POST'])
-def add_employee():
-    new_employee = request.json  # Data sent from frontend
-    employees.append(new_employee)
-    return jsonify({"message": "Employee added successfully!"}), 201
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)  # Not just '127.0.0.1'
